65435a99a787f6cbcefbaa7c98761bec4773e751
raphaelalkamim <83611337+raphaelalkamim@users.noreply.github.com>
2022-06-07 17:46:17 -0300

    home view implementation

---
+++ ghostProjetc/ViewController/HistoryViewController.swift
@@ -0,0 +1,15 @@
+//
+//  HistoryViewController.swift
+//  ghostProjetc
+//
+//  Created by Raphael Alkamim on 07/06/22.
+//
+
+import Foundation
+
+class HistoryViewController: ViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
