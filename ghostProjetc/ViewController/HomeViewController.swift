6c6a97f4d8107dad414ad475583db9705f25e753
raphaelalkamim <83611337+raphaelalkamim@users.noreply.github.com>
2022-06-08 18:09:58 -0300

    animation logo implemented

--- ghostProjetc/ViewController/HomeViewController.swift
+++ ghostProjetc/ViewController/HomeViewController.swift
@@ -10,13 +10,33 @@ import UIKit
 
 class HomeViewController: UIViewController {
     
+    @IBOutlet weak var logoView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         playSound(file: "opera.mp3")
+        oscillation(image: logoView)
     }
     
     @IBAction func startGameAction(_ sender: Any) {
         backgroundSound()
         playSecondSound(file: "ieIe.mp3")    }
+    
+    
+    func oscillation(image: UIImageView) {
+            let posA = CGPoint(x: 20, y: 188)
+            
+            var newPos: CGPoint
+            
+            if(__CGPointEqualToPoint(image.center, posA)) {
+                newPos = image.center
+            } else {
+                newPos = posA
+            }
+            
+            UIView.animate(withDuration: 2.0, delay: 0, options: [.repeat, .autoreverse], animations: {
+                image.center = newPos
+            })
+            
+        }
     
 }
